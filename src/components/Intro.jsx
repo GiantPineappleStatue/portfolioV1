63e447ad65f7167aef23f30caa5d1d17db14f1f0
Jerry Wu <jerrywu.hr@gmail.com>
2022-11-20 16:06:29 -0800

    updated

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,31 +1,69 @@
 import React from 'react';
+import styled from 'styled-components';
+import { keyframes } from 'styled-components';
+import { motion as m } from 'framer-motion';
 
 function Intro() {
-   return (
-      <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Forrest</h1>
-         <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
-         <p className="text-sm max-w-xl mb-6 font-bold">
-            I'm an indie full-stack developer and content creator building my
-            version of the digital world one step at a time. All coding projects
-            are built from the ground up, from planning and designing all the way
-            to solving real-life problems with code.
-            <br />
-            All video content is built the same way, from ideation and planning,
-            all the way to finalizing the content with artistic touches. I publish
-            that content on my YouTube channel{' '}
-            <a
-               href="https://youtube.com/fknight"
-               target="_blank"
-               className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600"
-               rel="noreferrer noopener"
-            >
-               ForrestKnight
-            </a>{' '}
-            to more than 400k subscribers.
-         </p>
+  const hue = keyframes`
+ from {
+   -webkit-filter: hue-rotate(0deg);
+ }
+ to {
+   -webkit-filter: hue-rotate(-360deg);
+ }
+`;
+  const AnimatedGradientText = styled.h1`
+    color: #f35626;
+    background-image: -webkit-linear-gradient(92deg, #f35626, #feab3a);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -webkit-animation: ${hue} 15s infinite linear;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial,
+      sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+    font-feature-settings: 'kern';
+    font-size: 48px;
+    font-weight: 700;
+    line-height: 48px;
+    overflow-wrap: break-word;
+    text-align: center;
+    text-rendering: optimizelegibility;
+    -moz-osx-font-smoothing: grayscale;
+  `;
+
+  return (
+    <m.div
+      initial={{ opacity: 0, y: '100%' }}
+      animate={{ opacity: 1, y: '0%' }}
+      transition={{ duration: 1, ease: 'easeOut' }}
+      exit={{ duration: 1, opacity: 0 }}
+      className="flex items-center justify-center flex-col text-center pt-40 pb-6"
+    >
+      <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">
+        <p className="text-xs text-left">Hi, I'm</p>
+        Jerry
+      </h1>
+      <p className="text-left">I'm a</p>
+      <div className="overflow-hidden">
+        <m.p
+          initial={{ opacity: 0, y: '100%' }}
+          animate={{ opacity: 1, y: '0%' }}
+          transition={{ delay: 0.75, duration: 0.75, ease: 'easeOut' }}
+          className="text-7xl md:text-7xl mb-3 font-medium pt-5"
+        >
+          <AnimatedGradientText>Fullstack Developer</AnimatedGradientText>
+        </m.p>
       </div>
-   )
+      <p className="text-sm max-w-xl mb-6 font-bold">
+        I'm a fullstack developer exploring the software engineering world.
+        <br />
+        This is the first version of my personal portfolio. Please take a look
+        around and let me know what you think about it!
+        <br />
+        The website is made with React, Framer Motion, Styled Components,
+        Tailwind CSS. Data is served on Strapi CMS.
+      </p>
+    </m.div>
+  );
 }
 
 export default Intro;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import portfolio from '../data/portfolio';
 import PortfolioItem from './PortfolioItem';
+import { motion as m } from 'framer-motion';
 
 function Portfolio() {
-   return (
-      <div className="flex flex-col md:flex-row items-center justify-center">
-         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {portfolio.map(project => (
-               <PortfolioItem 
-                  imgUrl={project.imgUrl}
-                  title={project.title}
-                  stack={project.stack}
-                  link={project.link}
-               />
-            ))}
-         </div>
-      </div>
-   )
+  return (
+    <div className="flex flex-col pt-20 md:flex-row items-center justify-center">
+      <m.div
+        initial={{ opacity: 0, x: '100%' }}
+        animate={{ opacity: 1, x: '0%' }}
+        transition={{ duration: 1, ease: 'easeOut' }}
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
+      >
+        {portfolio.map((project) => (
+          <PortfolioItem
+            imgUrl={project.imgUrl}
+            title={project.title}
+            stack={project.stack}
+            link={project.link}
+          />
+        ))}
+      </m.div>
+    </div>
+  );
 }
 
 export default Portfolio;
